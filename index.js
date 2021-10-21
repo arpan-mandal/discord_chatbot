00c65a3d069220f7d2e13cbba6c3ad6ed455d849
Jelli-Fish <92933926+Jelli-Fish@users.noreply.github.com>
2021-10-21 20:22:27 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+require('dotenv')
+const { Client,Util, Collection,MessageEmbed,Structures } = require("discord.js");
+const keepAlive = require('./server.js')
+keepAlive()
+async function errorEmbed(text,message){
+      const newembed = new Discord.MessageEmbed()
+      .setColor("#FF7676")
+      .setDescription(`**❌ | ${text} **`)
+       return message.channel.send(newembed);
+    }
+const Discord = require('discord.js');
+const client = new Client({
+    disableEveryone: true
+})
+const axios = require("axios")
+
+
+//=============================================
+const channel_id = "put channel ID here"
+//=============================================
+
+
+client.on('message', async (message) => {
+  if (!message.guild) return;
+  if (message.author.bot) return;
+  try {
+  if (message.channel.id != channel_id) return
+  let res = await axios.get(`http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=1&msg=${encodeURIComponent(message.content)}`);
+  message.reply(res.data.cnt);
+  } catch {
+   errorEmbed(`Bot error, please try again!`,message)
+   }
+})
+
+client.on('ready', async () => {
+    console.clear()
+    console.log(`${client.user.tag} is online!`)
+})
+
+client.login(process.env.TOKEN);
